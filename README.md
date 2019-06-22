a6642393dba24ba2133482d50f5981770946c832
Masmics <cherlocker3113@gmail.com>
2019-06-21 17:46:18 -0700

    tests passing, connects to client, DB not retaining data - debugging

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Server side of Neighbors Serve, an app that connects local volunteers with needed tasks in their community.
+Server side of Neighbors Serve, an app that connects local volunteers with needed tasks in their community. Written in JavaScript, Express/Node.js, mongoose/MongoDB, and Test-Driven Development using Jest and SuperTest. Users authenticated using Auth0.
---
+++ test/tasks.test.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const connect = require('../lib/utils/connect');
+const app = require('../lib/app');
+
+describe('tasks routes', () => {
+  beforeAll(() => {
+    return connect();
+  });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a task via POST route', () => {
+    return request(app)
+      .post('/api/v1/tasks/')
+      .send({ title: "POST-it", description: "Help me do a thing!" })
+      .then(res => {
+        console.log(res.text);
+        expect(res.body).toEqual({
+          _id: expect.any(String),          
+          title: "POST-it",
+          description: "Help me do a thing!",
+          __v: 0
+        });
+      });
+  });
+});
+
